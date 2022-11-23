f43f29ad18a2b1cf2c7746396c23dae7ba05411c
TigerCWShaw <cs4213@columbia.edu>
2022-11-23 16:25:53 -0500

    read the shift value from system

--- mm/expose_pgtbl.c
+++ mm/expose_pgtbl.c
@@ -1,8 +1,29 @@
 #include <linux/syscalls.h>
 #include <linux/expose_pgtbl.h>
 
+/*
+ * Investigate the page table layout.
+ *
+ * The page table layout varies over different architectures (eg. x86 vs arm).
+ * It also changes with the change of system configuration.  
+ *
+ * You need to implement a system call to get the page table layout information
+ * of the current system. Use syscall number 441.
+ * @pgtbl_info : user address to store the related infomation; note the
+ * normal page size can be inferred from the page_shift.
+ */
 SYSCALL_DEFINE1(get_pagetable_layout, struct pagetable_layout_info __user *, pgtbl_info)
 {
+	// if(!pgtbl_info)
+	// 	return -EINVAL;
+	struct pagetable_layout_info tmp;
+	tmp.pgdir_shift = PGDIR_SHIFT;
+	tmp.p4d_shift = P4D_SHIFT;
+	tmp.pud_shift = PUD_SHIFT;
+	tmp.pmd_shift = PMD_SHIFT;
+	tmp.page_shift = PAGE_SHIFT;
+	if(copy_to_user(pgtbl_info, &tmp, sizeof(struct pagetable_layout_info)))
+		return -EFAULT;
 	return 0;
 }
 
