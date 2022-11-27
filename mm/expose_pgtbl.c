4ab7622ea8b549a4e1f0efbc6730818c6f10ed41
Satwik290896 <vsaisatwik1996@gmail.com>
2022-11-26 21:11:08 -0500

    Fake Page tabltable creation

--- mm/expose_pgtbl.c
+++ mm/expose_pgtbl.c
@@ -69,6 +69,15 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 	unsigned long map_from_addr, map_to_addr;
 	pgprot_t flags;
 	int result = 0;
+	
+	pgd_t *fake_pgd;
+	p4d_t *fake_p4d;
+	pud_t *fake_pud;
+	pmd_t *fake_pmd;
+	p4d_t *p4d_e;
+	pud_t *pud_e;
+	pmd_t *pmd_e;
+	pte_t *pte_e;
 
 	if (!args)
 		return -EINVAL;
@@ -77,16 +86,44 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 
 	if (pid != -1)
 		from_task = find_task_by_vpid(pid);
-	if (!from_task || !current)
+	if (!from_task || !current) {
 		return -ESRCH;
-	from_mm = from_task->mm;
-	to_mm = current->mm;
+	}
+
+	from_mm = get_task_mm_expose(from_task);
+	if (!from_mm)
+	 	return -ESRCH;
+
+	to_mm = get_task_mm_expose(current);
+	if (!to_mm)
+	 	return -ESRCH;
 
 	/* TODO: copy the page tables */
+	fake_pgd->pgd = local_args.fake_pgd;
+	fake_p4d->pgd.pgd = local_args.fake_p4ds;
+	fake_pud->pud = local_args.fake_puds;
+	fake_pmd->pmd = local_args.fake_pmds;
+	map_to_addr = local_args.page_table_addr;
+	
+	p4d_e = p4d_alloc(to_mm, fake_pgd, map_to_addr);
+	pud_e = pud_alloc(to_mm, p4d_e, map_to_addr);
+	pmd_e = pmd_alloc(to_mm, pud_e, map_to_addr);
+	pte_e = pte_alloc_map(to_mm, pmd_e, map_to_addr);
+	
+	pud_e = pud_alloc(to_mm, fake_p4d, map_to_addr);
+	pmd_e = pmd_alloc(to_mm, pud_e, map_to_addr);
+	pte_e = pte_alloc_map(to_mm, pmd_e, map_to_addr);
+	
+	pmd_e = pmd_alloc(to_mm, fake_pud, map_to_addr);
+	pte_e = pte_alloc_map(to_mm, pmd_e, map_to_addr);
+	
+	pte_e = pte_alloc_map(to_mm, fake_pmd, map_to_addr);
+
+		
 
 	/* do the mapping from the VMA to page_table_addr*/
 	map_from_addr = local_args.begin_vaddr;
-	map_to_addr = local_args.page_table_addr;
+	
 	to_vma = find_vma(to_mm, local_args.page_table_addr);
 	flags = to_vma->vm_page_prot;
 
@@ -102,7 +139,7 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 		#ifdef CONFIG_ARM64
 		map_pfn = (pmd_val(READ_ONCE(*map_pmd)) & ((1UL << 48) - 1)) >> PAGE_SHIFT;
 		#else
-		map_pfn = pmd_pfn(map_pmd);
+		map_pfn = pmd_pfn(READ_ONCE(*map_pmd));
 		#endif
 
 		result = remap_pfn_range(to_vma, PAGE_ALIGN(map_to_addr),
