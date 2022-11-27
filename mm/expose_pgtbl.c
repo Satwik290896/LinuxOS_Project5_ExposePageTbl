e69383bd3b3276ecda10e5bd202b4b35bf92d8a5
Mohsin Rizvi <mkrizvi98@gmail.com>
2022-11-27 17:01:31 -0500

    checkpatch compliance

--- mm/expose_pgtbl.c
+++ mm/expose_pgtbl.c
@@ -27,37 +27,42 @@ struct mm_struct *get_task_mm_expose(struct task_struct *task)
  * Investigate the page table layout.
  *
  * The page table layout varies over different architectures (eg. x86 vs arm).
- * It also changes with the change of system configuration.  
+ * It also changes with the change of system configuration.
  *
  * You need to implement a system call to get the page table layout information
  * of the current system. Use syscall number 441.
- * @pgtbl_info : user address to store the related infomation; note the
+ * @pgtbl_info : user address to store the related information; note the
  * normal page size can be inferred from the page_shift.
  */
 SYSCALL_DEFINE1(get_pagetable_layout, struct pagetable_layout_info __user *, pgtbl_info)
 {
-	// if(!pgtbl_info)
-	// 	return -EINVAL;
+	/*
+	 * if(!pgtbl_info)
+	 * 	return -EINVAL;
+	 */
 	struct pagetable_layout_info tmp;
+
 	tmp.pgdir_shift = PGDIR_SHIFT;
 	tmp.p4d_shift = P4D_SHIFT;
 	tmp.pud_shift = PUD_SHIFT;
 	tmp.pmd_shift = PMD_SHIFT;
 	tmp.page_shift = PAGE_SHIFT;
-	if(copy_to_user(pgtbl_info, &tmp, sizeof(struct pagetable_layout_info)))
+	if (copy_to_user(pgtbl_info, &tmp, sizeof(struct pagetable_layout_info)))
 		return -EFAULT;
 	return 0;
 }
 
-/*struct expose_pgtbl_args {
-	unsigned long fake_pgd;
-	unsigned long fake_p4ds;
-	unsigned long fake_puds;
-	unsigned long fake_pmds;
-	unsigned long page_table_addr;
-	unsigned long begin_vaddr;
-	unsigned long end_vaddr;
-};*/
+/*
+ * struct expose_pgtbl_args {
+ *	unsigned long fake_pgd;
+ *	unsigned long fake_p4ds;
+ *	unsigned long fake_puds;
+ *	unsigned long fake_pmds;
+ *	unsigned long page_table_addr;
+ *	unsigned long begin_vaddr;
+ *	unsigned long end_vaddr;
+ * };
+ */
 
 SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *, args)
 {
@@ -69,7 +74,7 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 	unsigned long map_from_addr, map_to_addr;
 	pgprot_t flags;
 	int result = 0;
-	
+
 	pgd_t *fake_pgd;
 	p4d_t *fake_p4d;
 	pud_t *fake_pud;
@@ -86,17 +91,16 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 
 	if (pid != -1)
 		from_task = find_task_by_vpid(pid);
-	if (!from_task || !current) {
+	if (!from_task || !current)
 		return -ESRCH;
-	}
 
 	from_mm = get_task_mm_expose(from_task);
 	if (!from_mm)
-	 	return -ESRCH;
+		return -ESRCH;
 
 	to_mm = get_task_mm_expose(current);
 	if (!to_mm)
-	 	return -ESRCH;
+		return -ESRCH;
 
 	/* TODO: copy the page tables */
 	fake_pgd->pgd = local_args.fake_pgd;
@@ -104,7 +108,7 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 	fake_pud->pud = local_args.fake_puds;
 	fake_pmd->pmd = local_args.fake_pmds;
 	map_to_addr = local_args.page_table_addr;
-	
+
 	p4d_e = p4d_alloc(to_mm, fake_pgd, map_to_addr);
 	p4d_e->pgd.pgd = local_args.fake_p4ds;
 	pud_e = pud_alloc(to_mm, p4d_e, map_to_addr);
@@ -113,11 +117,10 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 	pmd_e->pmd = local_args.fake_pmds;
 	pte_e = pte_alloc_map(to_mm, pmd_e, map_to_addr);
 	pte_e->pte = map_to_addr;
-		
 
 	/* do the mapping from the VMA to page_table_addr*/
 	map_from_addr = local_args.begin_vaddr;
-	
+
 	to_vma = find_vma(to_mm, local_args.page_table_addr);
 	flags = to_vma->vm_page_prot;
 
@@ -140,100 +143,17 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 					 map_pfn, PAGE_SIZE, flags);
 		if (result != 0)
 			return result;
-		
+
 		map_from_addr = PAGE_ALIGN(map_from_addr + PAGE_SIZE);
 		map_to_addr = PAGE_ALIGN(map_to_addr + PAGE_SIZE);
 		to_vma = find_vma(to_mm, map_to_addr);
 	}
 
 	return 0;
-
-	/* struct task_struct *p_task; */
-	/* struct mm_struct *mm; */
-	/* struct expose_pgtbl_args *buf; */
-	/* u64 virtual_add_begin = 0; */
-	/* u64 virtual_add_end = OFFSET_MAX; */
-	/* u64 i = 0; */
-	/* struct vm_area_struct *vma; */
-	
-	/* if (!args) */
-	/* 	return -EINVAL; */
-
-	/* /\* kmalloc because size is large *\/ */
-	/* buf = kmalloc(BUF_SIZE*sizeof(struct expose_pgtbl_args), GFP_KERNEL); */
-	
-	/* if (!buf) */
-	/* 	return -ENOMEM; */
-
-	/* rcu_read_lock(); */
-	/* p_task = (pid == -1) ? current : find_task_by_vpid(pid); */
-	/* if (!p_task) { */
-	/* 	rcu_read_unlock(); */
-	/* 	return -ESRCH; */
-	/* } */
-	/* get_task_struct(p_task); */
-	/* rcu_read_unlock(); */
-
-	/* mm = get_task_mm_expose(p_task); */
-	
-	/* if (!mm) */
-	/* 	return -ESRCH; */
-
-	/* for (vma = mm->mmap; vma; vma = vma->vm_next) { */
-		
-	/* 	if (i < BUF_SIZE) { */
-	
-	/* 	buf[i].begin_vaddr	= vma->vm_start; */
-	/* 	buf[i].end_vaddr	= vma->vm_end; */
-	/* 	buf[i].fake_pgd		= 0; */
-	/* 	buf[i].fake_p4ds	= 0; */
-	/* 	buf[i].fake_puds	= 0; */
-	/* 	buf[i].fake_pmds	= 0; */
-	/* 	buf[i].page_table_addr	= 0; */
-		
-	/* 	u64 addr = vma->vm_start; */
-		
-	/* 	pgd_t *pgd		= pgd_offset(mm, addr); */
-		
-	/* 	if (!pgd) { */
-	/* 		buf[i].fake_pgd	= pgd->pgd; */
-	/* 		p4d_t *p4d	= p4d_offset(pgd, addr); */
-			
-	/* 		if (!p4d) { */
-	/* 			buf[i].fake_p4ds= p4d->pgd.pgd; */
-	/* 			pud_t *pud	= pud_offset(p4d, addr); */
-				
-	/* 			if (!pud) { */
-	/* 				buf[i].fake_puds= pud->pud; */
-	/* 				pmd_t *pmd	= pmd_offset(pud, addr); */
-				
-	/* 				if (!pmd) { */
-	/* 					buf[i].fake_pmds= pmd->pmd; */
-	/* 					pte_t *pte	= pte_offset_map(pmd, addr); */
-						
-	/* 					if (!pte) */
-	/* 						buf[i].page_table_addr	= pte->pte; */
-	/* 				} */
-	/* 			} */
-	/* 		} */
-	/* 	} */
-	/* 	} */
-		
-	/* 	i++; */
-	/* } */
-
-	
-	/* if (copy_to_user(args, buf, BUF_SIZE*sizeof(struct expose_pgtbl_args))) */
-	/* 	return -EFAULT; */
-	
-	/* kfree(buf); */
-	
-	/* return i; */
 }
 
 SYSCALL_DEFINE1(get_pa_contents, long, phys_addr)
 {
 	/* This may need to be tested and revised after 442 is done for further testing */
 	return *((int *)__va(phys_addr));
-	//return 0;
 }
