82027fe366419303c8e85c5f092fcf4ae89afdff
Mohsin Rizvi <mkrizvi98@gmail.com>
2022-11-27 09:59:33 -0500

    remap from the actual given address, not byte count

--- mm/expose_pgtbl.c
+++ mm/expose_pgtbl.c
@@ -136,7 +136,7 @@ SYSCALL_DEFINE2(expose_page_table, pid_t, pid, struct expose_pgtbl_args __user *
 		map_pfn = pmd_pfn(READ_ONCE(*map_pmd));
 		#endif
 
-		result = remap_pfn_range(to_vma, PAGE_ALIGN(map_to_addr),
+		result = remap_pfn_range(to_vma, map_to_addr,
 					 map_pfn, PAGE_SIZE, flags);
 		if (result != 0)
 			return result;
